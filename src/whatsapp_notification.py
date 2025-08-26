4ec418ec689fc26179b180323946090da0ea67ad
FelipeOG1 <feliog1234@gmail.com>
2025-08-25 20:30:04 -0600

    init user

--- src/whatsapp_notification.py
+++ src/whatsapp_notification.py
@@ -1,7 +1,7 @@
 #WHATSAPP DOCS:https://developers.facebook.com/docs/whatsapp/cloud-api/?locale=es_ES
-
+from criptoRequest import CriptoRequest
 class WhatsappNotification:
     
-    def __init__(self,phone_number):
+    def __init__(self):
         pass
-    
+       
